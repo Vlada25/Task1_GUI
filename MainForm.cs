5e679c7f5993ca6821caa947c1b55bd62bff72b7
Vlada <70849026+Vlada25@users.noreply.github.com>
2021-05-06 21:54:20 +0300

    refactor: removed the commented out non-part

--- MainForm.cs
+++ MainForm.cs
@@ -6,30 +6,6 @@ using EnumColors;
 
 namespace Task1_GUI
 {
-    /*
-    public enum Colors
-    {
-        [Display(Name = "#FF5B58")]
-        R = 0,
-
-        [Display(Name = "#FFAC40")]
-        O = 1,
-
-        [Display(Name = "#FFF650")]
-        Y = 2,
-
-        [Display(Name = "#8DFF70")]
-        G = 3,
-
-        [Display(Name = "#97FFF3")]
-        C = 4,
-
-        [Display(Name = "#7F8CFF")]
-        B = 5,
-
-        [Display(Name = "#A978FF")]
-        P = 6
-    }*/
     public partial class MainForm : Form
     {
         public MainForm()
@@ -277,14 +253,6 @@ namespace Task1_GUI
                 cardPanels2[i].BackColor = ColorTranslator.FromHtml(formColor);
             }
         }
-        /*
-        static string GetDisplayName(Colors color)
-        {
-            Type type = color.GetType();
-            var enumItem = type.GetField(color.ToString());
-            var attribute = enumItem?.GetCustomAttribute<DisplayAttribute>();
-            return attribute?.Name;
-        }*/
     }
 }
 
